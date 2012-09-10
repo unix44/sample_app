041b69241687323af959e7a515d2b476638ca49f
Artem A <artem.v.andreev@gmail.com>
2012-09-11 00:05:03 +0300

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+
+Factory.define :user do |user|
+    user.name                   "Artem A."
+    user.email                  "artem@mail.com"
+    user.password               "foobar"
+    user.password_confirmation  "foobar"
+end
